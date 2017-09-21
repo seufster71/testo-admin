5fad4bee871c2549f05533f521175ea5839f7255
edward.seufert <edward.seufert@TPA1WS01190.corp.autoclubfl.com>
2017-09-21 09:25:38 -0400

    init

---
+++ testo-admin/src/main/java/org/testo/TestoAdminApplication.java
@@ -0,0 +1,12 @@
+package org.testo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestoAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestoAdminApplication.class, args);
+	}
+}
